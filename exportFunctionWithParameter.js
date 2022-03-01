94ccce5e8c363e45a26b6ee6686e7d144991ab60
udaykiran <udaykiran.dhatrika@gmail.com>
2022-03-01 13:06:54 +0000

    Import and Export a Function with Parameter solution

---
+++ exportFunctionWithParameter.js
@@ -0,0 +1,11 @@
+// let number = 4;
+// function square(num) {
+//   return num * num;
+// }
+// module.exports = square;
+
+const square = (num) => {
+  return num * num;
+};
+
+module.exports = square;
