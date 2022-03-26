6b3c59ae1a0b825469ecc7a4c62e778ad7405b49
udaykiran-dhatrika <99610445+udaykiran-dhatrika@users.noreply.github.com>
2022-03-26 18:51:04 +0530

    Update exportFunctionWithParameter.js

--- exportFunctionWithParameter.js
+++ exportFunctionWithParameter.js
@@ -1,17 +1,18 @@
+// function declaration
 function square(num) {
   return num * num;
 }
 module.exports = square;
 
 
-
+// function expression
 let square = function (number) {
   return number * number;
 };
 module.exports = square;
 
 
-
+// arrow function
 const square = (num) => {
   return num * num;
 };
